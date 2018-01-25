59d6dedfd4d11496685cb1b687d6457dc5b11c21
DannyVanHalen <danny.van.halen.87@gmail.com>
2018-01-25 12:48:45 +0100

    proyecto-master

---
+++ urjc-academy/src/main/java/com/dad/urjcacademy/UrjcAcademyApplication.java
@@ -0,0 +1,12 @@
+package com.dad.urjcacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrjcAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrjcAcademyApplication.class, args);
+	}
+}
