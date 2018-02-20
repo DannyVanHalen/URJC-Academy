505e5a6de95eedaf4e4768a592f4d731b273856c
DannyVanHalen <danny.van.halen.87@gmail.com>
2018-02-20 18:30:39 +0100

    Actualización 20 de febrero 2018

--- urjc-academy/src/main/java/com/dad/urjcacademy/controller/AlumnoController.java
+++ urjc-academy/src/main/java/com/dad/urjcacademy/controller/AlumnoController.java
@@ -2,17 +2,43 @@ package com.dad.urjcacademy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.dad.urjcacademy.entity.Alumno;
+import com.dad.urjcacademy.entity.Usuario;
 import com.dad.urjcacademy.repository.AlumnoRepository;
 import com.dad.urjcacademy.repository.UsuarioRepository;
 
 @Controller
+@RequestMapping("/alumno")
 public class AlumnoController extends UsuarioController {
 
 	@Autowired
 	private UsuarioRepository usuarios;
 	
 	@Autowired
-	private AlumnoRepository repository;
+	private AlumnoRepository alumnos;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public String alumno(Model model, @PathVariable long id) {
+		
+		Alumno alumno = (Alumno) usuarios.findOne(id);
+		
+		if(alumno != null) {
+			
+			model.addAttribute("nombre", alumno.getNombre() + " " + alumno.getApellido());
+			model.addAttribute("asignaturas", alumno.getAsignaturas());
+			model.addAttribute("tutorias", alumno.getTutorias());
+			
+			return "alumno";
+			
+		}
+		
+		return "404";
+	
+	}
 	
 }
--- urjc-academy/src/main/java/com/dad/urjcacademy/controller/ProfesorController.java
+++ urjc-academy/src/main/java/com/dad/urjcacademy/controller/ProfesorController.java
@@ -2,17 +2,41 @@ package com.dad.urjcacademy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.dad.urjcacademy.entity.Profesor;
 import com.dad.urjcacademy.repository.ProfesorRepository;
 import com.dad.urjcacademy.repository.UsuarioRepository;
 
 @Controller
+@RequestMapping("/profesor")
 public class ProfesorController extends UsuarioController {
 
 	@Autowired
 	private UsuarioRepository usuarios;
 	
 	@Autowired
-	private ProfesorRepository repository;
+	private ProfesorRepository profesores;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public String profesor(Model model, @PathVariable long id) {
+		
+		Profesor profesor = (Profesor) usuarios.findOne(id);
+		
+		if(profesor != null) {
+			model.addAttribute("nombre", profesor.getNombre() + " " + profesor.getApellido());
+			model.addAttribute("asignaturas", profesor.getAsignaturas());
+			model.addAttribute("tutorias", profesor.getTutorias());
+			return "profesor";
+		}
+		
+		return "404";
+	
+	}
+	
+	
 		
 }
