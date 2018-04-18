2bd8c6a8ece855d3fba6efa1d158476be7fe0a7f
DannyVanHalen <danny.van.halen.87@gmail.com>
2018-04-19 01:07:16 +0200

    Invalidación de caché 1.0

--- urjc-academy/src/main/java/com/dad/urjcacademy/UrjcAcademyApplication.java
+++ urjc-academy/src/main/java/com/dad/urjcacademy/UrjcAcademyApplication.java
@@ -1,14 +1,31 @@
 package com.dad.urjcacademy;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
 
-
+/** Fase 4 -> Invalidacion de caché **/
+@EnableCaching
 @SpringBootApplication
 public class UrjcAcademyApplication {
 	
+	private static final Log LOG = LogFactory.getLog(UrjcAcademyApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UrjcAcademyApplication.class, args);
+	}
+	
+	
+	/*Fase 4 -> Invalidación de caché **/
+	@Bean
+	public CacheManager cacheManager() {
+		LOG.info("Activating cache...");
+		return new ConcurrentMapCacheManager("usuarios");
 	}
 	
 }
--- urjc-academy/src/main/java/com/dad/urjcacademy/repository/UsuarioRepository.java
+++ urjc-academy/src/main/java/com/dad/urjcacademy/repository/UsuarioRepository.java
@@ -2,21 +2,41 @@ package com.dad.urjcacademy.repository;
 
 import java.util.List;
 
+
+
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 
 import com.dad.urjcacademy.entity.Usuario;
 
+@Cacheable(cacheNames="usuarios")
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
 
 	/** Select **/
+	@Cacheable
+	Usuario findById(long id);
+	@Cacheable
 	Usuario findByLogin(String login);
+	@Cacheable
 	Usuario findByLoginAndPass(String login, String pass);
+	@Cacheable
 	Usuario findByMaiLogin(String maiLogin);
+	
+	@Cacheable
 	List<Usuario> findByRol(String rol);
 	//List<Usuario> findByRoles(String roles);
 	
+
+	
 	/** Delete **/
+	@Cacheable
 	Usuario deleteByMaiLogin(String maiLogin);
+	
+	
+	/**Insert**/
+	@CacheEvict(allEntries=true)
+	Usuario save(Usuario usuario);
 	
 	
 }
